290053b2abaf9490c6dc30f2c64438dc9e501408
zqb <zqb@endeavor.com>
2022-11-09 23:42:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zqbstart
+ * @create 2022-11-09 23:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
